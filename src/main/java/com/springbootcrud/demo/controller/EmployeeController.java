ded064e51ca8d432f824fdab87f5fadac8c42147
PavithPriyadharsan <pavithpriyadarshan222@gmail.com>
2024-03-21 13:46:54 +0530

    Initial commit

---
+++ src/main/java/com/springbootcrud/demo/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.springbootcrud.demo.controller;
+
+import com.springbootcrud.demo.entity.Employee;
+import com.springbootcrud.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    public EmployeeRepository employeeRepository;
+
+    @PostMapping("/employee")
+    public Employee saveEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable("id") String id){
+        return employeeRepository.getEmployeeById(id);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public String deleteEmployee(@PathVariable("id") String id){
+        return employeeRepository.delete(id);
+    }
+
+    @PutMapping("employee/{id}")
+    public String updateEmployee(@PathVariable("id") String id,@RequestBody Employee employee){
+        return employeeRepository.update(id,employee);
+    }
+}
