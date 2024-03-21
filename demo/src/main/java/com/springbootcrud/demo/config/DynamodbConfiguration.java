c1fd370b506e5afa6ae546e247edad9f569fd323
Pavith Priyadharsan S <146210938+PavithPriyadharsan@users.noreply.github.com>
2024-03-21 20:23:46 +0530

    Update DynamodbConfiguration.java

--- demo/src/main/java/com/springbootcrud/demo/config/DynamodbConfiguration.java
+++ demo/src/main/java/com/springbootcrud/demo/config/DynamodbConfiguration.java
@@ -5,6 +5,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import org.springframework.context.annotation.Bean;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.springframework.context.annotation.Configuration;
