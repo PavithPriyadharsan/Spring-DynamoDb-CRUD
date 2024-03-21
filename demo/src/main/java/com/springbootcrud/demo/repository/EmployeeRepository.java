ad90b1275810617ade177f58dcc982746c1494b3
Pavith Priyadharsan S <146210938+PavithPriyadharsan@users.noreply.github.com>
2024-03-21 20:16:10 +0530

    Update EmployeeRepository.java

--- demo/src/main/java/com/springbootcrud/demo/repository/EmployeeRepository.java
+++ demo/src/main/java/com/springbootcrud/demo/repository/EmployeeRepository.java
@@ -12,8 +12,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class EmployeeRepository {
 
+    private final DynamoDBMapper dynamoDBMapper;
     @Autowired
-    private DynamoDBMapper dynamoDBMapper;
+    public EmployeeRepository (DynamoDBMapper dynamoDBMapper){
+        this.dynamoDBMapper = dynamoDBMapper;
+    }
 
 
     public Employee save(Employee employee) {
